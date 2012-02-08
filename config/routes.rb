becca42f11b8ed6617e7777d2036a95d25444c58
David Witus <dwitus@gmail.com>
2012-02-07 19:04:03 -0800

    added sign-out and user name to sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Addressbook::Application.routes.draw do
 
   match "/auth/:provider/callback" => "sessions#create"
 
+  match "/signout" => "sessions#destroy", :as => :signout
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
